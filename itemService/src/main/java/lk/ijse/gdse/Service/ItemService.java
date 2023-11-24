a682f9cf30cfbc13f669bc2386c4329b263cc8b4
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-24 15:38:32 +0530

    add item service

---
+++ itemService/src/main/java/lk/ijse/gdse/Service/ItemService.java
@@ -0,0 +1,15 @@
+package lk.ijse.gdse.Service;
+
+import lk.ijse.gdse.microservice.Dto.ItemDto;
+import lk.ijse.gdse.microservice.exception.*;
+
+import java.util.List;
+
+public interface ItemService {
+    ItemDto searchitemBydesc(String desc) throws NotFoundException;
+    ItemDto searchItemByid(int id) throws NotFoundException;
+    int updateItem(ItemDto itemDto) throws UpdateFailException;
+    int addItem(ItemDto itemDto) throws CreateFailException;
+    void deleteItem(String desc) throws DeleteFailException;
+    List<ItemDto> getAll() throws NotFoundException;
+}
---
+++ itemService/src/main/java/lk/ijse/gdse/Service/impl/ItemServiceImpl.java
@@ -0,0 +1,65 @@
+package lk.ijse.gdse.Service.impl;
+
+import lk.ijse.gdse.microservice.Dto.ItemDto;
+import lk.ijse.gdse.microservice.Entity.Item;
+import lk.ijse.gdse.microservice.Repo.ItemRepo;
+import lk.ijse.gdse.microservice.Service.ItemService;
+import lk.ijse.gdse.microservice.exception.*;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+public class ItemServiceImpl implements ItemService {
+    @Autowired
+    ItemRepo itemRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Override
+    public ItemDto searchitemBydesc(String desc) throws NotFoundException {
+        Item byDesc = itemRepo.findByName(desc);
+        ItemDto map = modelMapper.map(byDesc, ItemDto.class);
+        return map;
+    }
+
+    @Override
+    public ItemDto searchItemByid(int id) throws NotFoundException {
+        Item byId = itemRepo.findById(id);
+        return modelMapper.map(byId,ItemDto.class);
+    }
+
+    @Override
+    public int updateItem(ItemDto itemDto) throws UpdateFailException {
+        Item byDesc = itemRepo.findByName(itemDto.getName());
+        if ( byDesc != null){
+            itemRepo.save(modelMapper.map(itemDto,Item.class));
+            return byDesc.getId();
+        }
+        return -1;
+    }
+
+    @Override
+    public int addItem(ItemDto itemDto) throws CreateFailException {
+        Item byDesc = itemRepo.findByName(itemDto.getName());
+        if ( byDesc == null){
+            itemRepo.save(modelMapper.map(itemDto,Item.class));
+            return byDesc.getId();
+        }
+        return byDesc.getId();
+    }
+
+    @Override
+    public void deleteItem(String desc) throws DeleteFailException {
+        Item byDesc = itemRepo.findByName(desc);
+        itemRepo.delete(byDesc);
+    }
+
+    @Override
+    public List<ItemDto> getAll() throws NotFoundException {
+        return null;
+    }
+}
