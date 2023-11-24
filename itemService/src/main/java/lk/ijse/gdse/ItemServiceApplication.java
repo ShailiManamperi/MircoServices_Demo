246d5a623fe6f35c292ca465f39a156c66f3f19c
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-24 15:39:05 +0530

    add item application

---
+++ itemService/src/main/java/lk/ijse/gdse/ItemServiceApplication.java
@@ -0,0 +1,25 @@
+package lk.ijse.gdse;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class ItemServiceApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ItemServiceApplication.class, args);
+    }
+    @Bean
+    public ModelMapper getMapper(){
+        return new ModelMapper();
+    }
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
+}
