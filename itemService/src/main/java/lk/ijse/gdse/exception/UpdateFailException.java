c7787caa78e5ba9295c49b1bf77976c613c48324
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-24 15:31:33 +0530

    add exception files

---
+++ itemService/src/main/java/lk/ijse/gdse/exception/UpdateFailException.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse.exception;
+
+public class UpdateFailException extends Exception{
+    public UpdateFailException(String message) {
+        super(message);
+    }
+    public UpdateFailException(String message, Throwable cause) {
+        super(message+" :( "+ cause.getMessage(), cause);
+    }
+}
---
+++ itemService/src/main/java/lk/ijse/gdse/exception/UserNotFoundException.java
@@ -0,0 +1,8 @@
+package lk.ijse.gdse.exception;
+
+public class UserNotFoundException extends Exception{
+    public UserNotFoundException(String message){
+        super(message);
+    }
+
+}
