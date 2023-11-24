cf515034a30a4b709362b88ad1b62064d8a44ba4
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-24 15:38:07 +0530

    add item repo

---
+++ itemService/src/main/java/lk/ijse/gdse/Repo/ItemRepo.java
@@ -0,0 +1,10 @@
+package lk.ijse.gdse.Repo;
+
+import lk.ijse.gdse.microservice.Entity.Item;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ItemRepo extends JpaRepository<Item,Integer> {
+    Item findByName(String desc);
+
+    Item findById(int id);
+}
