cdca7e3696610a7d4c8f8aca99fb8a5c38cb54b5
ShailiManamperi <shailimanamperi2002@gmail.com>
2023-11-25 09:49:17 +0530

    update item application

--- itemService/src/main/java/lk/ijse/gdse/ItemServiceApplication.java
+++ itemService/src/main/java/lk/ijse/gdse/ItemServiceApplication.java
@@ -6,9 +6,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@EnableWebMvc
 public class ItemServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(ItemServiceApplication.class, args);
@@ -22,4 +26,5 @@ public class ItemServiceApplication {
     public RestTemplate restTemplate(){
         return new RestTemplate();
     }
+
 }
